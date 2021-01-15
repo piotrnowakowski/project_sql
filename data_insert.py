a034bb2cdfd33c0d992df6abf1ce92506f21583c
piotrnowakowski <piotr98nowakowski@gmail.com>
2021-01-15 23:00:25 +0100

    1

--- data_insert.py
+++ data_insert.py
@@ -1,6 +1,7 @@
 import psycopg2
 import json
-def data_insert(data_toload):
+import pandas as pd
+
 tables_key_list = ["crime_table_total", "crime_state_average", "crime_average", "income_state_median",
                    "income_state_all_average"]
 with open('tables.json', 'r') as file:
@@ -8,3 +9,17 @@ with open('tables.json', 'r') as file:
     insert = insert_table['inserts_list']
     for i in tables_key_list:
         print(insert[i])
+
+house_income = pd.read_excel(r'Household Income.xls')
+
+
+def load_crime_rate():
+    for r in range(1, 52):
+        row = house_income.loc[r, :]
+        indices = [0, 1, 2]
+        for i in range(2, 62, 2):
+            indices.append(i)
+        data_toload = [row.iloc[index] for index in indices]
+        print(data_toload)
+
+
